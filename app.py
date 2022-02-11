657000a2313daeb4a06d9978a35742faa5f4ce4b
Timofey <tim20045@mail.ru>
2022-02-11 14:16:04 +0700

    Switch to rnn

--- app.py
+++ app.py
@@ -6,35 +6,13 @@ from mediapipe.framework.formats import landmark_pb2
 import mediapipe as mp
 import cv2
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
-
-def get_back():
-    rgb = [255, 255, 255]
-    
-    row = []
-    for i in range(192):
-        row.append(rgb)
-        
-    img = []
-    for j in range(192):
-        img.append(row)
-    
-    return np.array(img)
-    
-
-model_loaded = keras.models.load_model('gr_mobilenet2_model')
-img_generator = keras.preprocessing.image.ImageDataGenerator(
-        preprocessing_function=keras.applications.mobilenet_v2.preprocess_input
-    )
-#back = cv2.imread('background.jpg')
+model_loaded = keras.models.load_model('gr_rnn01_model')
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello friend!!!"
+    return "Hello, it is a gesture recogntion server!"
 
 @app.route('/letter')
 def recognize_gesture():
@@ -58,24 +36,15 @@ def recognize_gesture():
     h = maxY - minY
 
     maxLength = max(w, h)
-
-    for i in range(len(hand_landmarks.landmark)):
-        hand_landmarks.landmark[i].x = ((hand_landmarks.landmark[i].x + maxLength - maxX) * (192 - 2 * margin) / maxLength + margin) / 192
-        hand_landmarks.landmark[i].y = ((hand_landmarks.landmark[i].y - minY) * (192 - 2 * margin) / maxLength + margin) / 192
-
-    res_img = cv2.imread('background192.jpg')
-
-    mp_drawing.draw_landmarks(
-            res_img,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-
-    #res_img = cv2.flip(res_img, 1)
     
-    res_imgs = img_generator.flow(np.array([res_img.tolist()]))
-    pred = model_loaded.predict(res_imgs)
+    res_lmrks = [[]]
+    
+    for i in range(len(hand_landmarks.landmark)):
+        hand_landmarks.landmark[i].x = ((hand_landmarks.landmark[i].x - minX) * (200 - 2 * margin) / maxLength + margin) / 200
+        hand_landmarks.landmark[i].y = ((hand_landmarks.landmark[i].y - minY) * (200 - 2 * margin) / maxLength + margin) / 200
+        res_lmrks[0].append([hand_landmarks.landmark[i].x, hand_landmarks.landmark[i].y])
+    
+    pred = model_loaded.predict(res_lmrks)
     letter = np.argmax(pred,axis=1)
 
     return ' '.join(str(i) for i in pred[0])
